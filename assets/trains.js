13ba666caeac406b8cb07d50d5710d932ab44e3d
Gabriel Figarella <GFigarella@Gabriels-MacBook-Pro.local>
2017-12-20 23:30:30 -0500

    added basic html and js

---
+++ assets/trains.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPbC37ybEKLLfnTDa79E_speeKfVBegBk",
+    authDomain: "train-time-3f044.firebaseapp.com",
+    databaseURL: "https://train-time-3f044.firebaseio.com",
+    projectId: "train-time-3f044",
+    storageBucket: "train-time-3f044.appspot.com",
+    messagingSenderId: "356705036768"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+})
